f8763d36ebcb546d1c900e0aab08845b078c5fe2
Isaac-Amano <yishakamano2020@gmail.com>
2024-08-05 21:46:17 -0500

    removed post and delete routes from code to follow assignment instructions, GET route not working on postman anymore

--- server/routes/todo.router.js
+++ server/routes/todo.router.js
@@ -4,59 +4,16 @@ const pool = require('../modules/pool.js');
 
 // GET
 router.get('/', (req, res) => {
-    pool.query('SELECT * FROM tasks ORDER BY id')
-        .then(result => res.json(result.rows))
-        .catch(err => {
-            console.error('Error executing query', err.stack);
+    console.log('Initiating GET route');
+    const queryText = 'SELECT * FROM tasks ORDER BY id;';
+
+    pool.query(queryText)
+        .then((result) => {
+            console.log(result.rows);
+            res.send(200);
+        })
+        .catch((error) => {
+            console.error('error');
             res.sendStatus(400);
         });
 });
-
-// POST
-
-router.post('/', (req, res) => {
-    console.log('req.body:', req.body);
-
-    const { description } = req.body;
-    
-    const queryText = `
-      INSERT INTO "tasks" ("description") 
-      VALUES ($1);
-    `;
-    pool.query(queryText, [description])
-      .then(result => {
-        console.log(result);
-        res.sendStatus(201); 
-      })
-      .catch(error => {
-        console.error('Error');
-        res.sendStatus(400); 
-      });
-});
-
-
-//Delete route
-
-router.delete('/:id', (req, res) => {
-    console.log('req.params:', req.params);
-
-    const taskId = req.params.id;
-
-    const queryText = `
-      DELETE FROM "tasks" 
-      WHERE "id" = $1;
-    `;
-    pool.query(queryText, [taskId])
-      .then(result => {
-        console.log(result);
-        res.sendStatus(200); 
-      })
-      .catch(error => {
-        console.error('Error');
-        res.sendStatus(500); 
-      });
-});
-
-module.exports = router;
-
-
